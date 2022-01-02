b825bcc9f46306a2bea994cc08c7e95d34d8fef8
Chunsheng Yu <cyu.kore@gmail.com>
2022-01-02 11:54:33 -0600

    finish pcu upload page

--- app/helper/views.py
+++ app/helper/views.py
@@ -6,44 +6,50 @@ from werkzeug.utils import redirect
 from flask.helpers import url_for
 
 from app import helper, db
-from app.models import Case, CaseStatus
+from app.models import Case, CaseStatus, PCU_Lookup
 from app.helper.forms import PCUCSVUplodForm
 from app.helper import helper
 
 
 @helper.route('/upload_pcu', methods=['GET', 'POST'])
 @login_required
-def upload_csv():
+def upload_pcu():
     form = PCUCSVUplodForm()
 
     if form.validate_on_submit():
         filestream = form.file.data
         # filename = secure_filename(filestream.filename)
         df = pd.read_csv( filestream )
-        cases = []
-        for index, row in df[:5].iterrows():
-            # print(row['CP_NUM'], row['PartType'], row['Group'], row['Location'])
-            new_case = Case(
-                cp_num=row['CP_NUM'],
-                specimen_class=row['SpecimenClass'],
-                part_type=row['PartType'],
-                group_external_value=row['Group'],
-                part_description=row['PartDescription'],
-                block_count=row['BlockCount'],
-                doctor_code=row['DoctorCode'],
-                specialty=row['Specialty'],
-                location=row['Location'],
-                PCU=0,
-                status = CaseStatus.Created,
-                assignee_id = current_user.id
-
+        df = df.fillna('')
+        pcu_lookups = []
+        for index, row in df.iterrows():
+            jk_pcu = row['New JK PCU']
+            try:
+                float(jk_pcu)
+            except ValueError:
+                jk_pcu=0.0
+            
+            new_PCU_Lookup = PCU_Lookup(
+                part_type = row['PartType'],
+                jk_type = row['JK Type'],
+                new_jk_class = row['New JK Class'],
+                new_jk_pcu= jk_pcu,
+                comments = row['Comments']
             )
-            cases.append(new_case)
+            
+            pcu_lookups.append(new_PCU_Lookup)
+            # print(row['PartType'], row['New JK PCU'], row['Comments'], row['JK Type'])
 
-        print(len(cases))
-        db.session.add_all(cases)
+        print(len(pcu_lookups))
+        db.session.add_all(pcu_lookups)
         db.session.commit()
 
-        return redirect(url_for('main.index'))
+        return redirect(url_for('helper.upload_pcu_done'))
     
-    return render_template('workflow/upload_csv.html', title='Upload CSV', form=form)
+    return render_template('helper/upload_pcu.html', title='Upload CSV', form=form)
+
+
+@helper.route('/upload_pcu_done', methods=['GET', 'POST'])
+@login_required
+def upload_pcu_done():
+    return render_template('helper/upload_pcu_done.html', title='Upload PCU done')
